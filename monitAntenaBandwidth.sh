5eeb1168236a60311affb38ca61d63b40b94308f
Miguel Pérez-Francisco <mpf@openmailbox.org>
2017-07-24 12:26:25 +0200

    parametrized the file to store temporal data

--- monitAntenaBandwidth.sh
+++ monitAntenaBandwidth.sh
@@ -6,6 +6,8 @@ DATEPATTERN="Date_in_seconds:"
 
 host=$1
 IF=$2
+LOGFILE=$1.log
+
 if [ -z "$IF" ]; then
         IF=`ls -1 /sys/class/net/ | head -1`
 fi
@@ -16,21 +18,21 @@ echo $DATEPATTERN `date +%s`
 
 
 megas=2
-  ssh ubnt@$host "rm zzz; touch zzz;echo ${IF} > zzz;
-                  cat /sys/class/net/${IF}/statistics/tx_bytes>> zzz;
-                  echo \`adjtimex | grep tv_sec | awk '{print \$2}'\`\`adjtimex | grep tv_usec | awk '{print \$2}'\` >> zzz;
+  ssh ubnt@$host "echo ${IF} > ${LOGFILE};
+                  cat /sys/class/net/${IF}/statistics/tx_bytes>> ${LOGFILE};
+                  echo \`adjtimex | grep tv_sec | awk '{print \$2}'\`\`adjtimex | grep tv_usec | awk '{print \$2}'\` >> ${LOGFILE};
                   dd if=/dev/zero bs=4096 count=$((1024*${megas}/4));
-                  cat /sys/class/net/${IF}/statistics/tx_bytes>> zzz;
-                  echo \`adjtimex | grep tv_sec | awk '{print \$2}'\`\`adjtimex | grep tv_usec | awk '{print \$2}'\` >> zzz;" | cat > /dev/null
+                  cat /sys/class/net/${IF}/statistics/tx_bytes>> ${LOGFILE};
+                  echo \`adjtimex | grep tv_sec | awk '{print \$2}'\`\`adjtimex | grep tv_usec | awk '{print \$2}'\` >> ${LOGFILE};" | cat > /dev/null
 
-  scp ubnt@$host:zzz .
-  ssh ubnt@$host rm "zzz"
+  scp ubnt@$host:$LOGFILE .
+  ssh ubnt@$host rm "${LOGFILE}"
   
 
-  TPR=`sed -n '2p' < zzz`
-  TXPREV=`sed -n '3p' < zzz`
-  TPT=`sed -n '4p' < zzz`
-  TX=`sed -n '5p' < zzz`
+  TPR=`sed -n '2p' < $LOGFILE`
+  TXPREV=`sed -n '3p' < $LOGFILE`
+  TPT=`sed -n '4p' < $LOGFILE`
+  TX=`sed -n '5p' < $LOGFILE`
 
   let BWTX=$TX-$TXPREV
   let MICSECS=$((TPT-TPR))
